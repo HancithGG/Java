cd1af99b82c99505803114e59a34adb63c4ca9dc
HancithGG <learneasily.2534@gmail.com>
2026-02-15 17:15:46 +0530

    Add Employee, Developer, and Manager classes
    Implemented inheritance with Employee, Developer, and Manager classes.

---
+++ Concept based programs/Inheritance.java
@@ -0,0 +1,55 @@
+class Employee {
+
+    String name;
+    double basicSalary;
+
+    Employee(String name, double basicSalary) {
+        this.name = name;
+        this.basicSalary = basicSalary;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Basic Salary: " + basicSalary);
+    }
+}
+
+class Developer extends Employee {
+
+    double bonus;
+
+    Developer(String name, double basicSalary, double bonus) {
+        super(name, basicSalary);
+        this.bonus = bonus;
+    }
+
+    void totalSalary() {
+        System.out.println("Total Salary: " + (basicSalary + bonus));
+    }
+}
+
+class Manager extends Employee {
+
+    double allowance;
+
+    Manager(String name, double basicSalary, double allowance) {
+        super(name, basicSalary);
+        this.allowance = allowance;
+    }
+
+    void totalSalary() {
+        System.out.println("Total Salary: " + (basicSalary + allowance));
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Developer d = new Developer("Rahul", 50000, 10000);
+        d.display();
+        d.totalSalary();
+
+        Manager m = new Manager("Anil", 60000, 15000);
+        m.display();
+        m.totalSalary();
+    }
+}
