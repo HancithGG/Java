39e08afa0d2cbbc00dc6fc427fd158c52a927fc9
HancithGG <learneasily.2534@gmail.com>
2026-02-15 17:13:51 +0530

    Implement Calculator class with basic operations

---
+++ Concept based programs/Methods.java
@@ -0,0 +1,24 @@
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int square(int n) {
+        return n * n;
+    }
+
+    int power(int base, int exp) {
+        if (exp == 0)
+            return 1;
+        return base * power(base, exp - 1);
+    }
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+
+        System.out.println("Sum: " + c.add(10, 5));
+        System.out.println("Square: " + c.square(4));
+        System.out.println("Power: " + c.power(2, 3));
+    }
+}
