9b75510e626ec82a3fc379e388bc52490c5e163f
HancithGG <learneasily.2534@gmail.com>
2026-02-18 20:32:26 +0530

    Enhance Payment subclasses with additional methods
    Added walletname, cardname, and appname methods to their respective payment classes.

--- Concept based programs/Polymorphism.java
+++ Concept based programs/Polymorphism.java
@@ -1,3 +1,4 @@
+//Consider the following Java program where a base class Payment is extended by three subclasses: CashPayment, CardPayment, and UPIPayment. Each subclass overrides the processPayment() method.
 class Payment {
     void processPayment() {
         System.out.println("Processing payment");
@@ -8,17 +9,26 @@ class CashPayment extends Payment {
     void processPayment() {
         System.out.println("Cash payment successful");
     }
+    void walletname(){
+        System.out.println("brown leather");
+    }
 }
 
 class CardPayment extends Payment {
     void processPayment() {
         System.out.println("Card payment successful");
     }
+    void cardname(){
+        System.out.println("sbi");
+    }
 }
 
 class UPIPayment extends Payment {
     void processPayment() {
         System.out.println("UPI payment successful");
+    }
+    void appname(){
+        System.out.println("phonePe");
     }
 }
 
@@ -29,11 +39,14 @@ class Main {
 
         p = new CashPayment();
         p.processPayment();
+        p.walletname(); // unable to access though parent reference
 
         p = new CardPayment();
         p.processPayment();
+        p.cardname();    // unable to access though parent reference
 
         p = new UPIPayment();
         p.processPayment();
+        p.appname();    // unable to access though parent reference
     }
 }
